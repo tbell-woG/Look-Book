605fb990290cc80355eaf4ace4237878b0baeec3
Merlin Rock <rock.merlin@yahoo.com>
2021-02-12 10:29:17 -0500

    test

---
+++ client/src/utils/API.js
@@ -0,0 +1,21 @@
+// import React from "react";
+// import axios from "axios";
+
+
+// // Gets all artwork
+// function App() {
+//     const artwork =() => {
+// axios.get("https://www.rijksmuseum.nl/api/nl/collection/SK-C-5?key=5Gv0w7lO").then(
+//     res) => {
+//         console.log(res);
+//     }
+// )
+// }
+   
+// return(
+//     <div>
+//         <button onClick={artwork}>Get Artwork</button>
+//     </div>
+// )
+
+// export default App;
