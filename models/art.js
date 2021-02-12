79841062f3f397b3d599c2b3178cc2364fcb06d9
Brandon <brandonbgrayson@gmail.com>
2021-02-12 13:06:11 -0500

    added Art schema based on data being pulled from api

--- models/art.js
+++ models/art.js
@@ -1,2 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+const artSchema = new Schema({
+    id: {type: Number},
+    title: {type: String},
+    author: {type: String},
+    date: {type: Date, default: Date.now}
+})
+
+const Art = mongoose.model("Art", artSchema);
+
+module.exports = Art;
