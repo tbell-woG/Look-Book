45a23da914afeaf6257dc75a51a8c5b75ed08152
Brandon <brandonbgrayson@gmail.com>
2021-02-13 17:16:36 -0500

    card added to load response from api

--- client/src/pages/Art.js
+++ client/src/pages/Art.js
@@ -1,39 +1,51 @@
-import React, { useState, useEffect} from "react"
+import React, { useState, useEffect } from "react"
 import API from "../utils/API"
 
 function Art() {
     const [art, setArt] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         API.getArtFromMuseum()
-        .then(res => {
+            .then(res => {
 
-            setArt(res.data.artObjects)
-        })
-    },[])
+                setArt(res.data.artObjects)
+            })
+    }, [])
 
-    
 
-        const artCardMap = art.map(el => {
-            console.log(el)
-            return (
-            <h1>{el.title}</h1>
-          
-            )
+
+    const artCardMap = art.map((el, index) => {
+        console.log(el)
+        console.log(index)
+        // return (
+        //     <h1>{el.title}</h1>
+        //     )
         })
 
 
     return (
-        <div>
+                <div>
 
-         
+                    Welcome To The Art Page
 
-            Welcome To The Art Page
+                    Check out our Sweet Titles
 
-            Check out our Sweet Titles
+            {/* {artCardMap} */}
 
-            {artCardMap}
-        </div>
+
+            <div class="card" style={{width: "18rem"}}>
+                <img src="..." class="card-img-top" alt="..."/>
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+            </div>
+
+
+
+
+                </div>
     )
 }
 
